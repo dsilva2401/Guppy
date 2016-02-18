1e0d8bc6400a4092a0c360cc14b72dc662161e75
Diego Silva <dsilva2401@gmail.com>
2016-02-18 10:13:21 -0500

    Server error method updated

--- src/front/modules/app-admin/app/app.config.js
+++ src/front/modules/app-admin/app/app.config.js
@@ -30,6 +30,15 @@
 						templateUrl: '/front/modules/app-admin/users/registered.html',
 						controller: 'usersRegisteredController'
 					})
+				.state('surveys', {
+					url: '/surveys',
+					template: '<div class="cover" ui-view></div>'
+				})
+					.state('surveys.create', {
+						url: '/create',
+						templateUrl: '/front/modules/app-admin/surveys/create.html',
+						controller: 'surveysCreateController'
+					})
 
 
 	});
